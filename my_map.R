2cc98cf607e0454cb9f7293d4a478bd43240df8f
John Sugden <7507362+johnsug@users.noreply.github.com>
2020-05-19 10:40:11 -0500

    Update my_map.R

--- my_map.R
+++ my_map.R
@@ -6,6 +6,18 @@ library(ggplot2)
 windows.options(antialias="cleartype")
 ## coorinates from https://mapstogpx.com/
 
+## to do:
+# future project, map (roughly) everywhere I've been
+# * powder springs to ellijay
+# * oklahoma trip...
+# 
+# ## shade: lived (utah, idaho, georgia, texas, missouri, kansas) -- just the county?
+# ## maybe shade states I HAVE NOT been to (WA, MT, ND, SD, WI, LA, IA, OH, WV, VA, NC, SC, DE, PA, RI, NH, VT, ME) 18/51 ~35%
+)
+# ## dots where traveled for work (SF, Reno, Oklahoma, Dallas, Austin, DC, Vegas)
+# ## dots where run races (Atlanta, Chattanooga/Nashville, KC, St. George, Buckeye, Portland)
+
+
 ## custom formula to parse coordinates
 parse_gpx <- function(data){
   require(data.table)
@@ -45,12 +57,22 @@ fl2 <- parse_gpx(fl2)
 
 fl <- rbind(fl1, fl2)
 
+## does rounding corrupt? not really, not much difference between 2 and 3 decimal places
+fl[, lat:=round(lat, 2)]
+fl[, long:=round(long, 2)]
+fl <- unique(fl)
+
 ## quick plot
 usa <- map_data("state")
+not <- data.table(usa)
+not <- not[region %in% c("washington", "montana", "north dakota", "south dakota", "wisconsin", "louisiana", "iowa", "ohio", "west virginia", "virginia", 
+                         "north carolina", "south carolina", "delaware", "pennsylvania", "rhode island", "new hampshire", "vermont", "maine")]
+not[, .N, by=region]
 
 ggplot() + 
+  geom_polygon(data=not, aes(x=long, y=lat, group=group), fill="green", alpha=.1) + 
   geom_polygon(data=usa, aes(x=long, y=lat, group=group), size=1, color=5, fill=5, alpha=.15) + 
-  geom_point(data=fl, aes(x=long, y=lat), color="dodgerblue", size=.5) + ## takes a while
+  geom_point(data=fl, aes(x=long, y=lat), color=6, size=.8) + ## takes a while
   theme_minimal() + 
   theme(legend.position="none", 
         axis.text=element_blank(), 
