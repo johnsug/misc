f656130dd0ec5b26a38452d23d57caa15fa78d2b
John Sugden <7507362+johnsug@users.noreply.github.com>
2020-05-20 16:13:14 -0500

    Update my_map.R

--- my_map.R
+++ my_map.R
@@ -8,12 +8,9 @@ windows.options(antialias="cleartype")
 
 ## to do:
 # future project, map (roughly) everywhere I've been
-# * powder springs to ellijay
 # * oklahoma trip...
 # 
 # ## shade: lived (utah, idaho, georgia, texas, missouri, kansas) -- just the county?
-# ## maybe shade states I HAVE NOT been to (WA, MT, ND, SD, WI, LA, IA, OH, WV, VA, NC, SC, DE, PA, RI, NH, VT, ME) 18/51 ~35%
-)
 # ## dots where traveled for work (SF, Reno, Oklahoma, Dallas, Austin, DC, Vegas)
 # ## dots where run races (Atlanta, Chattanooga/Nashville, KC, St. George, Buckeye, Portland)
 
@@ -47,20 +44,32 @@ parse_gpx <- function(data){
   return(data)
 }
 
-## import
-fl1 <- data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200519_132301.gpx"))
-fl2 <- data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200519_135528.gpx"))
-
-## parse gpx
-fl1 <- parse_gpx(fl1)
-fl2 <- parse_gpx(fl2)
-
-fl <- rbind(fl1, fl2)
+## import and parse gpx
+g01 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200516.gpx")))
+g02 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200526.gpx")))
+g03 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200532.gpx")))
+g04 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200542.gpx")))
+g05 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200558.gpx")))
+g06 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200611.gpx")))
+g10 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200642.gpx")))
+g11 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200651.gpx")))
+g13 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200709.gpx")))
+g14 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200715.gpx")))
+g15 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_200724.gpx")))
+g21 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_202704.gpx")))
+g22 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_202942.gpx")))
+g23 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_203026.gpx")))
+g24 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_203103.gpx")))
+g25 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_203456.gpx")))
+g31 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_205646.gpx")))
+g32 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_205900.gpx")))
+g33 <- parse_gpx(data.table(read.csv("C:/Users/JohnSugden/OneDrive - Caravan Health/John_Sugden_Private/mapstogpx20200520_210356.gpx")))
+gpx <- rbind(g01, g02, g03, g04, g05, g06, g10, g11, g13, g14, g15, g21, g22, g23, g24, g25, g31, g32, g33)
 
 ## does rounding corrupt? not really, not much difference between 2 and 3 decimal places
-fl[, lat:=round(lat, 2)]
-fl[, long:=round(long, 2)]
-fl <- unique(fl)
+gpx[, lat:=round(lat, 3)]
+gpx[, long:=round(long, 3)]
+gpx <- unique(gpx)
 
 ## quick plot
 usa <- map_data("state")
@@ -70,14 +79,15 @@ not <- not[region %in% c("washington", "montana", "north dakota", "south dakota"
 not[, .N, by=region]
 
 ggplot() + 
-  geom_polygon(data=not, aes(x=long, y=lat, group=group), fill="green", alpha=.1) + 
+  geom_polygon(data=not, aes(x=long, y=lat, group=group), fill=5, alpha=.2) + 
   geom_polygon(data=usa, aes(x=long, y=lat, group=group), size=1, color=5, fill=5, alpha=.15) + 
-  geom_point(data=fl, aes(x=long, y=lat), color=6, size=.8) + ## takes a while
+  geom_point(data=gpx, aes(x=long, y=lat), color="royalblue", size=.8) + 
   theme_minimal() + 
   theme(legend.position="none", 
         axis.text=element_blank(), 
         panel.grid=element_blank()) + 
   labs(x="", y="") + 
   coord_fixed(1.3)
-  
+
+#g + geom_point(data=g10, aes(x=long, y=lat), color=3, size=.8) + labs(title="g10")
 
